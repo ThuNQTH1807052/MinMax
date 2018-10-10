68f94b469a8a4b3498f1c8b4049b11b8d2772bd7
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-11 00:16:51 +0700

    commit lan cuoi

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int minMang(int mangPhanTu[], int size){
+    int min = mangPhanTu[0];
+    for (int j = 0; j < size; j++) {
+        if(mangPhanTu[j] < min){
+            min = mangPhanTu[j];
+        }
+    }
+    return min;
+}
+int maxMang(int mangPhanTu[], int size){
+    int max = mangPhanTu[0];
+    for (int j = 0; j < size; j++) {
+        if(max < mangPhanTu[j]){
+            max = mangPhanTu[j];
+        }
+    }
+    return max;
+}
 int main() {
     int size;
-    printf("Nhap so luong cac phan tu trong mang (<=10): \n");
-    scanf("%d", &size);
+    do {
+        printf("Nhap so luong cac phan tu trong mang (so phan tu phai nho hon hoac bang 10): \n");
+        scanf("%d", &size);
+    }
+    while(size > 10);
     int mangPhanTu[size];
-
     for (int i = 0; i < size; i++) {
         printf("Vui lòng nhập giá trị cho phần tử thứ %d\n", i + 1);
         scanf("%d", &mangPhanTu[i]);
@@ -20,16 +40,15 @@ int main() {
     scanf("%d", &choice);
     switch(choice){
         case 1:
-
-            printf("So nho nhat la: %d\n");
+            printf("So nho nhat la: %d\n",minMang(mangPhanTu,size));
             break;
         case 2:
-            printf("So lon nhat la: %d\n");
+            printf("So lon nhat la: %d\n",maxMang(mangPhanTu,size));
             break;
         default:
             printf("Hen gap lai!\n");
             break;
     }
-
     return 0;
 }
+
